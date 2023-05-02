beb04adfffccd6929bf91482fe483f02132ca67f
Richard Poelderl <richard.poelderl@gmail.com>
2023-05-02 18:14:52 +0200

    add next-18next

---
+++ next-i18next.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+const config = {
+  i18n: {
+    locales: ["en", "de"],
+    defaultLocale: "en",
+    domains: [
+      {
+        domain: "localhost:3001",
+        defaultLocale: "en",
+        http: true,
+      },
+      {
+        domain: "de.localhost:3001",
+        defaultLocale: "de",
+        http: true,
+      },
+    ],
+    localeDetection: false,
+  },
+  localePath: path.resolve("./public/static/locales"),
+  defaultNS: "website",
+  debug: true
+};
+
+module.exports = config;
